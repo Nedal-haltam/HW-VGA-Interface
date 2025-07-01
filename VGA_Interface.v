c051773bd8383a6dac90b5fbd7f5d93fc23be3ae
Nedal Abu Haltam <nedalhaltam1@gmail.com>
2025-07-01 18:47:45 +0300

    write on putty terminal using FPGA through Arduino

--- VGA_Interface.v
+++ VGA_Interface.v
@@ -37,39 +37,76 @@ module VGA_Interface(
 	inout 		    [15:0]		ARDUINO_IO,
 	inout 		          		ARDUINO_RESET_N
 );
+`define BUFFER_LEN 6
+`define CLK_BIT 15
 
 wire VGA_CLK, DLY_RST;
 
-Reset_Delay reset
-(
-	.iCLK(MAX10_CLK1_50),
-	.oRESET(DLY_RST)
-);
+wire reset = ~KEY[0];
+reg [7:0] DataOut;
+wire DataTrigger;
 
-VGA_PLL vga_pll
-(
-	.areset(~DLY_RST),
-	.inclk0(MAX10_CLK1_50),
-	.c0(VGA_CLK)
-);
+wire [7:0] data [0 : `BUFFER_LEN - 1];
+reg [31:0] index;
+reg [24:0] halfclk;
 
-VGA_controller VGA_CTRL
-(
-	.iVGA_CLK(VGA_CLK), 
-	.iRST_n(DLY_RST), 
-	.AutoMan_StaticImage(1'b0),
-	// .AdvanceCursor(~KEY[0]),
-	// .KeyboardInput(SW[7:0]),
-	.AdvanceCursor(ARDUINO_IO[8]),
-	.KeyboardInput(ARDUINO_IO[7:0]),
+always@(posedge ADC_CLK_10) begin
+	halfclk <= halfclk + 1'b1;
+end
 
-	.r_data(VGA_R),
-	.g_data(VGA_G),
-	.b_data(VGA_B),
+always@(posedge halfclk[`CLK_BIT], posedge reset) begin
+	if (reset) begin
+		index = 0;
+		DataOut = 0;
+	end
+	else if (index <= `BUFFER_LEN) begin
+		DataOut = data[index];
+		index = index + 1'b1;
+	end
+end
+
+
+assign data[0] = "a";
+assign data[1] = "b";
+assign data[2] = "c";
+assign data[3] = "d";
+assign data[4] = "e";
+assign data[5] = "\n";
+
+assign DataTrigger = (index <= `BUFFER_LEN) ? reset | ~halfclk[`CLK_BIT] : 1'b0;
+assign ARDUINO_IO[7:0] = DataOut;
+assign ARDUINO_IO[8] = DataTrigger;
+
+// Reset_Delay reset
+// (
+// 	.iCLK(MAX10_CLK1_50),
+// 	.oRESET(DLY_RST)
+// );
+
+// VGA_PLL vga_pll
+// (
+// 	.areset(~DLY_RST),
+// 	.inclk0(MAX10_CLK1_50),
+// 	.c0(VGA_CLK)
+// );
+
+// VGA_controller VGA_CTRL
+// (
+// 	.iVGA_CLK(VGA_CLK), 
+// 	.iRST_n(DLY_RST), 
+// 	.AutoMan_StaticImage(1'b0),
+// 	// .AdvanceCursor(~KEY[0]),
+// 	// .KeyboardInput(SW[7:0]),
+// 	.AdvanceCursor(ARDUINO_IO[8]),
+// 	.KeyboardInput(ARDUINO_IO[7:0]),
+
+// 	.r_data(VGA_R),
+// 	.g_data(VGA_G),
+// 	.b_data(VGA_B),
 	
-	.oHS(VGA_HS),
-	.oVS(VGA_VS)
-);
+// 	.oHS(VGA_HS),
+// 	.oVS(VGA_VS)
+// );
 
 assign ARDUINO_RESET_N = 1'b1;
 
